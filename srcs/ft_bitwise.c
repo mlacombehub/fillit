beffa3c5e0b30d3f252d11dc709677da1e954f77
Xavier <barthex@gmail.com>
2019-01-23 21:03:58 +0100

    bitwise ops first draft

---
+++ srcs/ft_bitwise.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_bitwise.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xbarthe <xbarthe@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/23 19:07:31 by xbarthe           #+#    #+#             */
+/*   Updated: 2019/01/23 20:54:21 by xbarthe          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int    ft_bitcompact(int bitmino)
+{
+    // AND mask : x = x then can shift
+    return (bitmino);
+}
+
+int     ft_charToBit(chqr *c)
+{
+    int k;
+    int intBit;
+
+    intBit = 0;
+    k = 0;
+    while (char[k] != '\0')
+    {
+        k++;
+    }
+    return intBit;
+}
