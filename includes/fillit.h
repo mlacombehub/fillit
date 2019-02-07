8aeb5668ef6aa9f954efc4475987a83362a11bb3
Xavier Barthe <xbarthe@student.42.fr>
2019-02-07 17:05:19 +0100

    new main : Windset is everything

--- includes/fillit.h
+++ includes/fillit.h
@@ -6,20 +6,27 @@
 /*   By: xbarthe <xbarthe@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/13 14:23:20 by mlacombe          #+#    #+#             */
-/*   Updated: 2019/01/25 18:52:15 by xbarthe          ###   ########.fr       */
+/*   Updated: 2019/02/07 16:31:48 by xbarthe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FILLIT_H
 # define FILLIT_H
-# define FILE_LENGTH (26 * 21 + 1)
+# define FILE_MAX_LENGTH (26 * 21)
 
 # include "../libft/includes/libft.h"
 
+/*
+** n_piece : the printed char (order of reading a b c ...)
+**
+*/
+
+
 typedef struct	s_piece
 {
-	char		*n_piece;
-	int			oposition;
+	int			num_piece;
+	char 		*tetriminchar[22];
+	int			origposition;
 	int			width;
 	int			height;
 	uint16_t	referencebinary;
--- srcs/main.c
+++ srcs/main.c
@@ -5,80 +5,13 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: xbarthe <xbarthe@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/01/13 14:22:23 by mlacombe          #+#    #+#             */
-/*   Updated: 2019/01/25 18:03:15 by xbarthe          ###   ########.fr       */
+/*   Created: 2019/02/07 16:15:30 by xbarthe           #+#    #+#             */
+/*   Updated: 2019/02/07 17:04:25 by xbarthe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fillit.h"
-#include <fcntl.h>
-
-int		main(int argc, char **argv)
-{
-	char			*map[FILE_LENGTH + 1];
-	int				fd;
-	int				originmap;
-	int				binmap;
-	int				finalmap;
-
-	if (argc == 2)
-		if (fd == open(argv[1], O_RDONLY))
-		{
-			originmap = read(fd, map, FILE_LENGTH);
-			map[FILE_LENGTH + 1] = '\0';
-			binmap = ft_checking(originmap, map);
-			finalmap = ft_print_map(binmap);
-			close(originmap);
-		}
-		else
-			ft_putendl_fd(2, "error");
-	else
-		ft_putendl_fd(2, "usage: ./fillit [input_file]");
-	return (0);
-}
-
-int		ft_checking(int originmap, char *map)
-{
-	char		buff[22];
-	char		checker;
-	int			binmap;
-	t_piece		piece;
-
-	*piece.n_piece = 'A';
-	while (originmap != *piece.n_piece++ * 21)
-	{
-		*buff = originmap + (*piece.n_piece - 1) * 21;
-		checker = buff[0];
-		if ((buff[4] && buff[9] && buff[14] && buff[19] && buff[20]) == '\n' && piece.n_piece <= 27)
-		{
-			while (checker++ != buff[20])
-				if (checker != '#' || checker != '.' || checker != '\n')
-					return (0);
-			piece = ft_piece_creator(buff, piece.n_piece);
-			//map[(*piece.n_piece - 1) * 20] = ft_char_to_bin(*buff, piece);
-		}
-		else if (buff[0] == '\0')
-			return (binmap = *map);
-		else
-			return (0);
-	}
-	free(map);
-}
-
-int		ft_char_to_bin(char *buff, t_piece piece)
-{
-	int		i;
-	char	*ret;
-
-	i = 1;
-	while (buff[i++] != '\0')
-	{
-		if (!(buff = ft_verification(buff)))
-			return (0);
-		piece.binary = ft_buff_to_binary(buff);
-	}
-	return (ret);
-}
+#include <fcntl.h>// in libft ??
 
 int		ft_verification(char *buff)
 {
@@ -104,51 +37,50 @@ int		ft_verification(char *buff)
 	return ((n_conn == 6 || n_conn == 8) ? 1 : 0);
 }
 
-uint16_t		*ft_buff_to_binary(char *buff)
+int	ft_check_originalfeed(char *feed, int feedsize)
 {
-	size_t	i;
-	size_t	j;
-	char	binmap[16];
+	int	k;
 
-	i = 19;
-	j = 16;
-	while (i--)
-		if (buff[i] == '#')
-			binmap[j--] = 1;
-		else if (buff[i] == '.')
-			binmap[j--] = 0;
-	// while ((binmap[15] && binmap[14] && binmap[13] && binmap[12]) == 0)
-	// {
-	// 	binmap[15] << 16;
-	// 	binmap[14] << 15;
-	// 	binmap[13] << 14;
-	// 	binmap[12] << 13;
-	// }
-	// while ((binmap[15] && binmap[11] && binmap[7] && binmap[3]) == 0)
-	// 	(binmap[15] && binmap[11] && binmap[7] && binmap[3]) << 4;
-	return (binmap);
+	if ( feedsize / 21 < 1 || feedsize / 21 > 26 || feedsize % 21 != 0)
+		return (0);
+	while (*feed)
+		{
+			k = 0;
+			while (k++ <= 21)
+			{
+				if (k + 1 % 5 == 0 && feed[k] != '\n')
+						return (0);
+				else if (k + 1 % 5 != 0 && (feed[k] != '#' || feed[k] !='.'))
+						return (0);
+			}
+			if (!ft_verification(feed))
+				return (0);
+			*feed += 21;
+		}
+	return (1);
 }
 
-t_piece	ft_piece_creator(char *buff, char letter)
+int	ft_puterror(char *msg)
 {
-	t_piece	piece;
-	int i;
-
-	i = 0;
-	piece.n_piece = letter;
-	piece.width = 1;
-	piece.height = 1;
-	while (buff[i++])
-	{
-		if ((buff[i] == buff[i + 1]) && buff[i] != '.')
-			piece.width++;
-		else if ((buff[i] == buff [i + 5]) && buff[i] != '.')
-			piece.height++;
-	}
-	return (piece);
+	ft_putendl_fd(msg, 2);
+	return (0);
 }
 
-int		ft_print_map()
+
+
+int		main(int argc, char **argv)
 {
+	int				fd;
+	int				feedsize;
+	char			feed[FILE_MAX_LENGTH + 1];
+
+	if (argc != 2)
+		return(ft_puterror("usage: ./fillit [input_file]"));
+	if ((fd = open(argv[1], O_RDONLY)) <= 0)
+		return(ft_puterror("error"));
+	feedsize = read(fd, feed, FILE_MAX_LENGTH);
+	feed[feedsize + 1] = '\0';
+	if (ft_check_originalfeed(feed, feedsize) == 0)
+		return(ft_puterror("error"));
 
 }
