6ac663c6d5e2b588f8f4b8feabe68a636b03f362
Xavier Barthe <xbarthe@student.42.fr>
2019-02-14 12:17:07 +0100

    solver update

--- solver.c
+++ solver.c
@@ -6,30 +6,87 @@
 /*   By: xbarthe <xbarthe@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/25 17:22:38 by mlacombe          #+#    #+#             */
-/*   Updated: 2019/02/13 18:38:39 by xbarthe          ###   ########.fr       */
+/*   Updated: 2019/02/14 12:16:53 by xbarthe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./includes/fillit.h"
 
+
 /*
-** We check if the piece can be placed on map
+** cut the piece in 4 lines
+** put it on the map
+** try and put on line 1, then 1 2 3
+** on first fail increment rightwise
+** on end of map (over the side) increment downwise
 */
 
-void		ft_piececanfit(t_piece piece, int shift_column, int shift_line, int map_side, uint16_t map)
+int ft_piececanbeput(t_piece *tab, int num_piece, uint16_t *map, int map_side)
 {
+	int	mask_increment;
+	int shift_column;
+	int shift_line;
 
-	map = NULL;
+	mask_increment = 0;
+	shift_column = 0;
+	shift_line = 0;
+	while ((tab[num_piece].size.y + shift_line) < map_side)
+	{
+		while ((tab[num_piece].size.x + shift_column) < map_side && shift_line++)
+			while (mask_increment < 4)
+				if ((((0b1111 << mask_increment * 4) & tab[num_piece].compbin) << shift_column)	& map[shift_line + mask_increment++])
+				{
+					if (mask_increment == 3)
+						return(1);
+				}
+				else
+				{
+					shift_column++;
+					mask_increment = 0;
+					break;
+				}
+		shift_column = 0;
+		shift_line++;
+	}				// save position of piece on map
+	return (0);
+}
+
+
+/*
+** Put each line one by one at position shifted by column and line as passed
+** Adding the bits on the line is made with OR
+*/
+
+void	ft_putpiece(t_piece *tab, int num_piece, uint16_t *map, int shift_column, int shift_line)
+{
+	int	mask_increment;
+
+	mask_increment = 0;
+	while (mask_increment++ < 4)
+		map[shift_line + mask_increment] = map[shift_line + mask_increment] | (((0b1111 << mask_increment * 4) & tab[num_piece].compbin) << shift_column);
+	tab[num_piece].origpos.x = shift_column;
+	tab[num_piece].origpos.y = shift_line;
 }
 
 /*
-**  We remove the piece if it is not possible to make it go inside the map
+** Remove each line one by one at position shifted by column and line as passed
+** Removing the bits on the line is made with XOR
 */
 
-void		ft_remover(t_piece *tab, uint16_t map)
+void	ft_removepiece(t_piece *tab, int num_piece, uint16_t *map)
 {
+	int	mask_increment;
+	int shift_column;
+	int shift_line;
 
-	map = NULL;
+	mask_increment = 0;
+	shift_column = tab[num_piece].origpos.x;
+	shift_line = tab[num_piece].origpos.y;
+
+	while (mask_increment++ < 4)
+		map[shift_line + mask_increment] = map[shift_line + mask_increment] ^ (((0b1111 << mask_increment * 4) & tab[num_piece].compbin) << shift_column);
+	tab[num_piece].origpos.x = 0;
+	tab[num_piece].origpos.y = 0;
 }
 
 /*
@@ -52,82 +109,25 @@ int			ft_placer(t_piece *tab, size_t map_side, uint16_t *map, int pieceqty)
 	shift_column = 0;
 	shift_line = 0;
 
-	/*
-	** cut the piece in 4 lines
-	** put it on the map
-	** try and put on line 1, then 1 2 3
-	** on first fail increment rightwise
-	*/
-
-	while (num_piece < pieceqty)
-		if (tab[num_piece].size.x + shift_column < map_side )
-			while (mask_increment < 4)
-				if ( ( ((0b1111 << mask_increment * 4) & tab[num_piece].compbin) << shift_column) & map[l + mask_increment])
-						mask_increment++;
-				else
-				{
-					shift_column++;
-					break;
-				}
-				i
-
-	if (ft_piececanfit(tab[num_piece], shift_column, shift_line, map_side, map))
-	{ putpiece()
-		i++;
-		place (num_piece++)
+	if (ft_piececanbeput(tab, num_piece,*map , map_side).x = -1)
+	{
+		ft_putpiece(tab, num_piece, map, shift, shift_column)
+		num_piece++;
 	}
 	else
 	{
-	removepiece(numpiece--)
+	removepiece(numpiece--);
 	}
 
-	function piececanbeput(piece)
+	function piececanbeput(piece);
 
 
-	while piececanbeput i
-		putpiece
-		try next piece
+	while (piececanbeput[num_piece])
+		putpiece;
+		try next piece;
 
 	num_piece;
-	}
 }
-
-
-/*
-*
-*/
-
-int ft_piececanbeput(t_piece *tab, int num_piece, uint16_t *map, int map_side)
-{
-	int shift_line;
-	int shift_column;
-	int	mask_increment;
-
-	mask_increment = 0;
-	shift_column = 0;
-	shift_line = 0;
-	while ((tab[num_piece].size.y + shift_line) < map_side)
-	{
-		while ((tab[num_piece].size.x + shift_column) < map_side )
-		{
-			while (mask_increment < 4)
-				if ((((0b1111 << mask_increment * 4) & tab[num_piece].compbin) << shift_column) & map[l + mask_increment++])
-				{
-					if (mask_increment == 3)
-						return(1);
-				}
-				else
-				{
-					shift_column++;
-					mask_increment = 0;
-					break;
-				}
-		}
-		shift_line++;
-	}				// save position of piece on map
-	return (0);
-}
-
 
 /*
 ** We take the map, and put the pieces inside
